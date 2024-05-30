b5581f67b13a0d1adea549c2f0bc4c3e3b42dc59
SapicoAJ <162416738+SapicoAJ@users.noreply.github.com>
2024-05-30 15:38:38 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById('form');
-const form = document.getElementById(form);
-const form = document.getElementById(form);
-const form = document.getElementById(form);
-const form = document.getElementById(form);
-
-
-
